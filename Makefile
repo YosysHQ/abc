YosysHQ/abc

commit b9dfb992c78e3c8786dee303e4a9994d46b6990a

(1) Makefile: added a shared library target, (2) no longer compile the main function as part of libabc.a

Makefile
@@ -139,6 +139,8 @@ OBJ := \
 	$(patsubst %.c, %.o,  $(filter %.c, $(SRC)))  \
 	$(patsubst %.y, %.o,  $(filter %.y, $(SRC)))
 
+LIBOBJ := $(filter-out src/base/main/main.o,$(OBJ))
+
 DEP := $(OBJ:.o=.d)
 
 # implicit rules
@@ -186,10 +188,14 @@ $(PROG): $(OBJ)
 	@echo "$(MSG_PREFIX)\`\` Building binary:" $(notdir $@)
 	$(VERBOSE)$(LD) -o $@ $^ $(LIBS)
 
-lib$(PROG).a: $(OBJ)
+lib$(PROG).a: $(LIBOBJ)
 	@echo "$(MSG_PREFIX)\`\` Linking:" $(notdir $@)
 	$(VERBOSE)ar rv $@ $?
 	$(VERBOSE)ranlib $@
+
+lib$(PROG).so: $(LIBOBJ)
+	@echo "$(MSG_PREFIX)\`\` Linking:" $(notdir $@)
+	$(VERBOSE)$(CXX) -shared -o $@ $^ $(LIBS)
 
 docs:
 	@echo "$(MSG_PREFIX)\`\` Building documentation." $(notdir $@)

commit 28fe854ab5b6da9f4af5995b4d0e8f2653450230

Makefile: Respect LDFLAGS when linking shared library
LDFLAGS is taken into account when linking the executable, but not the
shared library, which is inconsistent and may cause issues if customized
settings are passed through LDFLAGS (for example, LTO). Let's include
LDFLAGS in the linking command.

Fixes: b9dfb992c78e ("(1) Makefile: added a shared library target, (2) no longer compile the main function as part of libabc.a")
Signed-off-by: Yao Zi <ziyao@disroot.org>

Makefile
@@ -231,7 +231,7 @@ lib$(PROG).a: $(LIBOBJ)
 
 lib$(PROG).so: $(LIBOBJ)
 	@echo "$(MSG_PREFIX)\`\` Linking:" $(notdir $@)
-	$(VERBOSE)$(CXX) -shared -o $@ $^ $(LIBS)
+	$(VERBOSE)$(CXX) -shared -o $@ $^ $(LDFLAGS) $(LIBS)
 
 docs:
 	@echo "$(MSG_PREFIX)\`\` Building documentation." $(notdir $@)